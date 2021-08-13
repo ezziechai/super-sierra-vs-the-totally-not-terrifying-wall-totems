62de62c04825fa2e57cbd1125746d9fe383f5dd7
ezziechai <85370053+ezziechai@users.noreply.github.com>
2021-08-13 17:01:46 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,115 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (sierra.isHittingTile(CollisionDirection.Bottom) || sierra.isHittingTile(CollisionDirection.Left) || sierra.isHittingTile(CollisionDirection.Right)) {
+        sierra.vy = -200
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    sierra.setImage(leftFacingImg)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    game.over(true)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    sierra.setImage(rightFacingImg)
+})
+let sierra: Sprite = null
+let leftFacingImg: Image = null
+let rightFacingImg: Image = null
+let rightSwordOutImg = img`
+    . . . . . . . f f . . . . . . . 
+    . . . . f f f f 2 f f . . . . . 
+    . . f f e e e e f 2 f f . . . . 
+    . f f e e e e e f 2 2 f f . . . 
+    . f e e e e f f e e e e f . . . 
+    . f f f f f e e 2 2 2 2 e f . . 
+    f f f e 2 2 2 f f f f e 2 f . . 
+    f f f f f f f f e e e f f f . . 
+    f e f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 b f d d e f . . . 
+    . . f e e e 4 d d d e e . c . . 
+    . . . f 2 2 2 2 e e d d e c c c 
+    . . . f 4 4 4 e 4 4 d d e c d d 
+    . . . f f f f f e e e e . c c c 
+    . . f f f f f f f f . . . c . . 
+    . . f f f . . f f . . . . . . . 
+    `
+let leftSwordOutImg = img`
+    . . . . . . . f f . . . . . . . 
+    . . . . . f f 2 f f f f . . . . 
+    . . . . f f 2 f e e e e f f . . 
+    . . . f f 2 2 f e e e e e f f . 
+    . . . f e e e e f f e e e e f . 
+    . . f e 2 2 2 2 e e f f f f f . 
+    . . f 2 e f f f f 2 2 2 e f f f 
+    . . f f f e e e f f f f f f f f 
+    . . f e e 4 4 f b e 4 4 e f e f 
+    . . . f e d d f b 4 d 4 e e f . 
+    . . c . e e d d d 4 e e e f . . 
+    c c c e d d e e 2 2 2 2 f . . . 
+    d d c e d d 4 4 e 4 4 4 f . . . 
+    c c c . e e e e f f f f f . . . 
+    . . c . . . f f f f f f f f . . 
+    . . . . . . . f f . . f f f . . 
+    `
+rightFacingImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . f f e e e e f 2 f . . . . 
+    . . f f e e e e f 2 2 2 f . . . 
+    . . f e e e f f e e e e f . . . 
+    . . f f f f e e 2 2 2 2 e f . . 
+    . . f e 2 2 2 f f f f e 2 f . . 
+    . f f f f f f f e e e f f f . . 
+    . f f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 1 f d d e f . . . 
+    . . f e e e e e d d d f . . . . 
+    . . . . f 4 d d e 4 e f . . . . 
+    . . . . f e d d e 2 2 f . . . . 
+    . . . f f f e e f 5 5 f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f . . . f f f . . . . 
+    `
+leftFacingImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . f 2 f e e e e f f . . . 
+    . . . f 2 2 2 f e e e e f f . . 
+    . . . f e e e e f f e e e f . . 
+    . . f e 2 2 2 2 e e f f f f . . 
+    . . f 2 e f f f f 2 2 2 e f . . 
+    . . f f f e e e f f f f f f f . 
+    . . f e e 4 4 f b e 4 4 e f f . 
+    . . . f e d d f 1 4 d 4 e e f . 
+    . . . . f d d d e e e e e f . . 
+    . . . . f e 4 e d d 4 f . . . . 
+    . . . . f 2 2 e d d e f . . . . 
+    . . . f f 5 5 f e e f f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f f . . . f f . . . . 
+    `
+sierra = sprites.create(rightFacingImg, SpriteKind.Player)
+controller.moveSprite(sierra, 100, 0)
+tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(sierra)
+tiles.placeOnTile(sierra, tiles.getTileLocation(4, 31))
+scene.setBackgroundColor(10)
+sierra.ay = 350
+game.onUpdate(function () {
+    if (sierra.isHittingTile(CollisionDirection.Right) && sierra.vy > 0) {
+        sierra.ay = 0
+        sierra.vy = 15
+        sierra.setImage(rightSwordOutImg)
+    } else if (sierra.isHittingTile(CollisionDirection.Left) && sierra.vy > 0) {
+        sierra.ay = 0
+        sierra.vy = 15
+        sierra.setImage(leftSwordOutImg)
+    } else {
+        sierra.ay = 350
+        if (sierra.image == leftSwordOutImg) {
+            sierra.setImage(leftFacingImg)
+        }
+        if (sierra.image == rightSwordOutImg) {
+            sierra.setImage(rightFacingImg)
+        }
+    }
+})
